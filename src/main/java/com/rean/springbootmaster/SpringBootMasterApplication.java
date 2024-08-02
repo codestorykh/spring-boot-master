4d0c59148a1a71846689fdf3e87db1f1cfb79cf8
D.Theara <dawntheara.me@gmail.com>
2024-08-02 19:44:14 +0700

    Spring Data JPA

--- src/main/java/com/rean/springbootmaster/SpringBootMasterApplication.java
+++ src/main/java/com/rean/springbootmaster/SpringBootMasterApplication.java
@@ -1,11 +1,32 @@
 package com.rean.springbootmaster;
 
+import com.rean.springbootmaster.todo.model.Todo;
+import com.rean.springbootmaster.todo.repository.TodoRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 public class SpringBootMasterApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMasterApplication.class, args);
+	}
+
+	@Autowired
+	private TodoRepository todoRepository;
+
+	@PostConstruct
+	public void init() {
+		List<Todo> todos = todoRepository.findAllByCompleted(true);
+
+		Optional<Todo> todo = todoRepository.findFirstById(1);
+
+		List<Todo> todosByTitle = todoRepository.findAllByTitleContaining("title");
+
+		List<Todo> todosByDescription = todoRepository.findAllByDescriptionContaining("description");
 	}
 }
--- src/main/java/com/rean/springbootmaster/todo/repository/TodoRepository.java
+++ src/main/java/com/rean/springbootmaster/todo/repository/TodoRepository.java
@@ -2,9 +2,6 @@ package com.rean.springbootmaster.todo.repository;
 
 import com.rean.springbootmaster.todo.model.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +12,13 @@ import java.util.Optional;
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
     Optional<Todo> findFirstById(long id);
-    Optional<Todo> findFirstByIdAndAndCompleted(long id, boolean completed);
+
+    List<Todo> findAllByCompleted(boolean completed);
+
+    List<Todo> findAllByTitleContaining(String title);
+
+    List<Todo> findAllByDescriptionContaining(String description);
+
+    List<Todo> findAllByTitleContainingAndDescriptionContaining(String title, String description);
 
 }
